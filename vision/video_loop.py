649ac20a0fa755fa39ec5969fdb1aa031c8589c2
Marat Gizetdinov <gizetdinov.m.f@gmail.com>
2024-04-30 14:37:16 +0300

    add looper over video which draw boxes over plates

--- vision/video_loop.py
+++ vision/video_loop.py
@@ -1,16 +1,24 @@
+import numpy as np
 import imageio.v3 as iio
+from licenseplates import get_license_plates, model
+from PIL import Image, ImageDraw
 
-# read a single frame
-frame = iio.imread(
-    "imageio:cockatoo.mp4",
-    index=42,
-    plugin="pyav",
-)
+source = '/home/marat/Videos/vlc-record-2024-04-30-12h56m23s-Street Sounds 2  _Road Nosie_.mp4-.mp4'
+dest = '/home/marat/Videos/overley.mp4'
 
-# bulk read all frames
-# Warning: large videos will consume a lot of memory (RAM)
-frames = iio.imread("imageio:cockatoo.mp4", plugin="pyav")
 
-# iterate over large videos
-for frame in iio.imiter("imageio:cockatoo.mp4", plugin="pyav"):
-    print(frame.shape, frame.dtype)
+fps = iio.immeta(source, plugin="pyav")["fps"]
+
+with iio.imopen(dest, "w", plugin="pyav") as out_file:
+    out_file.init_video_stream(codec='vp9', fps=15)
+
+
+    for frame in iio.imiter(source, plugin="pyav"):
+        frame_image = Image.fromarray(frame)
+        draw = ImageDraw.Draw(frame_image)
+        frame_res = model(frame)
+        for i in frame_res.pred:
+            if len(i) > 0:
+                x1, y1, x2, y2 = i[0, :4]
+                draw.rectangle((x1, y1, x2, y2), outline=(0, 255, 0), width=5)
+        out_file.write_frame(np.array(frame_image))
